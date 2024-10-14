e5c6b48877681be9bc1ec0e75c0339d53230729e
Jacob <jacob.gregg@hotmail.com>
2024-10-14 15:44:53 +1000

    returns input

--- algorithms/compress.py
+++ algorithms/compress.py
@@ -33,7 +33,7 @@ def my_compressor(in_bytes: bytes) -> bytes:
     baseline general purpose compression tool.
     """
     # Implement me!
-    return bytes
+    return in_bytes
 
 def my_decompressor(compressed_bytes: bytes) -> bytes:
     """
@@ -42,7 +42,7 @@ def my_decompressor(compressed_bytes: bytes) -> bytes:
     Once again, we've just used xz.
     """ 
     # Implement me!
-    return bytes
+    return compressed_bytes
 
 def compress_file(in_path: str, out_path: str) -> None:
     """
